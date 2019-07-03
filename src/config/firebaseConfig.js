7f12353eadde6ebfb5f5e971890d6e15390aa0db
Paulo Chaves <opaulochaves@gmail.com>
2019-07-03 20:51:32 -0300

    add firebase config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9riJPKkP5BdTyG6VVqspERiJZaEE6SI8",
+  authDomain: "pc-marioplan.firebaseapp.com",
+  databaseURL: "https://pc-marioplan.firebaseio.com",
+  projectId: "pc-marioplan",
+  storageBucket: "",
+  messagingSenderId: "424636267256",
+  appId: "1:424636267256:web:76edf6cee8ead4cb"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
