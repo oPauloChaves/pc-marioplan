83b6a27693a4538b2acfac69465b71e53d7256e7
Paulo Chaves <opaulochaves@gmail.com>
2019-07-03 14:34:21 -0300

    nav links and css

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -4,7 +4,7 @@ const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span class="card-title">{project.title}</span>
+        <span className="card-title">{project.title}</span>
         <p>{project.text}</p>
         <p className="grey-text">{project.date}</p>
       </div>
