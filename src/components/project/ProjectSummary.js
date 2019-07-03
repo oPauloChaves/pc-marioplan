7ff992d5df65e49757e2e3a4cffc5ad7d2c3f6a2
Paulo Chaves <opaulochaves@gmail.com>
2019-07-03 20:15:21 -0300

    read fake date from redux

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({ project }) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>{project.text}</p>
+        <p>{project.content}</p>
         <p className="grey-text">{project.date}</p>
       </div>
     </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,35 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "help me find peach",
+      content:
+        "Dolor et minima dignissimos alias sapiente quia. Eius veniam aliquid esse veniam quam.",
+      date: "3rd September, 2am"
+    },
+    {
+      id: "2",
+      title: "collect all the stars",
+      content:
+        "Dolor et minima dignissimos alias sapiente quia. Eius veniam aliquid esse veniam quam.",
+      date: "3rd September, 2am"
+    },
+    {
+      id: "3",
+      title: "egg hunt with yoshi",
+      content:
+        "Dolor et minima dignissimos alias sapiente quia. Eius veniam aliquid esse veniam quam.",
+      date: "3rd September, 2am"
+    },
+    {
+      id: "4",
+      title: "go to next stage",
+      content:
+        "Dolor et minima dignissimos alias sapiente quia. Eius veniam aliquid esse veniam quam.",
+      date: "3rd September, 2am"
+    }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
