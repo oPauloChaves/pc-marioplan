4e203e722786e3585d1fa18ac16dd16efed21420
Paulo Chaves <opaulochaves@gmail.com>
2019-07-04 16:30:53 -0300

    Cloud Function Firestore Triggers and Notification component v36

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp(functions.config().firebase);
+
+// triggered by a http request (eg. browser)
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello, Firebase!");
 });
+
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("Notification Added", doc));
+};
+
+// triggers whenever a new project is created in the collection `projects`
+exports.projectCreated = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+    // notify user new project was added
+    const project = doc.data();
+    const notification = {
+      content: "Added a new project",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return createNotification(notification);
+  });
